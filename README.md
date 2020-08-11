34f123428e27108d95174f4ae2abafe8573cb968
EcranBleu <nclochez@gmail.com>
2020-08-11 10:47:51 +0200

    file upload

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # PDF_watermarker
+
+Uses PyPDF2 module.
+
+Takes a file as input and creates a watermarked output file.
---
+++ main.py
@@ -0,0 +1,17 @@
+import PyPDF2
+import sys
+
+file_to_wtr = sys.argv[1]
+
+
+template = PyPDF2.PdfFileReader(open(file_to_wtr, 'rb'))
+watermark = PyPDF2.PdfFileReader(open('wtr.pdf', 'rb'))
+output = PyPDF2.PdfFileWriter()
+
+for i in range(template.getNumPages()):
+  page = template.getPage(i)
+  page.mergePage(watermark.getPage(0))
+  output.addPage(page)
+
+  with open('watermarked_output.pdf', 'wb') as file:
+    output.write(file)
